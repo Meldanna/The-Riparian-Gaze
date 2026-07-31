38278574dfe7c5aec86d60c6044c9b590baa1d75
Meldanna <meldanna57@gmail.com>
2026-07-31 21:50:25 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+import { extension_settings, getContext } from "../../../extensions.js";
+
+const MODULE_NAME = "st-timeline-memory";
+
+jQuery(async () => {
+    console.log(`[${MODULE_NAME}] 插件已加载`);
+    // 待会我们往这里加树状图面板、总结逻辑、跳转逻辑
+});
